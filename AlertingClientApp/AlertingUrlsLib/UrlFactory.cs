65b9ea39cc886a1f0dd0a064e35a3e9207a1c39c
venkatesh.prasad <venkateshprasad29@gmail.com>
2019-09-17 19:09:41 +0530

    Added URL lib

---
+++ AlertingClientApp/AlertingUrlsLib/UrlFactory.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertingUrlsLib
+{
+    public class UrlFactory
+    {
+        private const string BaseUrl = "http://localhost:62455/";
+        public string Id { get; set; } = null;
+        public string BedsUrl
+        {
+            get
+            {
+                if (Id == null)
+                    return BaseUrl + "api/Beds";
+                else
+                {
+                    var url = BaseUrl + "api/Beds/" + Id;
+                    Id = null;
+                    return url;
+                }
+
+            }
+        }
+        public string MonitorsUrl
+        {
+            get
+            {
+                if (Id == null)
+                    return BaseUrl + "api/Monitors";
+                else
+                {
+                    var url = BaseUrl + "api/Monitors/" + Id;
+                    Id = null;
+                    return url;
+                }
+
+            }
+        }
+        public string PatientsUrl
+        {
+            get
+            {
+                if (Id == null)
+                    return BaseUrl + "api/Patients";
+                else
+                {
+                    var url = BaseUrl + "api/Patients/" + Id;
+                    Id = null;
+                    return url;
+                }
+
+            }
+        }
+        public string RandomVitalsGeneratorUrl { get { return BaseUrl + "api/Utility"; } }
+        public string RandomIdGeneratorUrl
+        {
+            get
+            {
+                var url = BaseUrl + "api/Utility/" + Id;
+                Id = null;
+                return url;
+            }
+        }
+        public string IcuAssociationUrl
+        {
+            get
+            {
+                if (Id == null)
+                    return BaseUrl + "api/Icu/beds";
+                else
+                {
+                    var url = BaseUrl + "api/Icu/beds/" + Id;
+                    Id = null;
+                    return url;
+                }
+
+            }
+        }
+        public string WritePatientVitalsUrl
+        {
+            get
+            {
+                var url = BaseUrl + "api/Icu/beds/" + Id + "/values";
+                Id = null;
+                return url;
+            }
+        }
+        public string PatientVitalsValidationUrl
+        {
+            get
+            {
+                var url = BaseUrl + "api/Icu/beds/" + Id + "/validate";
+                Id = null;
+                return url;
+            }
+        }
+        public string IcuAlerts { get { return BaseUrl + "api/Icu/beds/alerts"; } }
+
+    }
+}
