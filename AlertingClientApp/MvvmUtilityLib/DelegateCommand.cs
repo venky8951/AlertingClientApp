e6226b40ce6d1f9aa71998a78fe8d8918aa1b440
venkatesh.prasad <venkateshprasad29@gmail.com>
2019-09-18 11:48:21 +0530

    Mvvm Utility Lib added

---
+++ AlertingClientApp/MvvmUtilityLib/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MvvmUtilityLib
+{
+    public class DelegateCommand : ICommand
+    {
+
+
+        public event EventHandler CanExecuteChanged;
+        private Action<Object> _actionMethodAddress;
+        private Func<object, bool> _canExceuteMethodAddress;
+        public DelegateCommand(Action<object> actionMethodAddress, Func<Object, bool> canExceuteMethodAddress)
+        {
+            this._actionMethodAddress = actionMethodAddress;
+            this._canExceuteMethodAddress = canExceuteMethodAddress;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExceuteMethodAddress.Invoke(parameter);
+        }
+
+
+
+        public void Execute(object parameter)
+        {
+            this._actionMethodAddress.Invoke(parameter);
+        }
+    }
+}
